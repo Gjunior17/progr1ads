4c3b5b8126544a389bad3520824c8d59f0f2fd90
Gjunior17 <61851805+Gjunior17@users.noreply.github.com>
2020-03-13 19:49:48 -0300

    Delete TAREFA 1 GERALDO.py

--- TAREFA 1 GERALDO.py
+++
@@ -1,4 +0,0 @@
-valor_litro = float(input("Informe o valor do litro: "))
-valor_reais = float(input("Quanto quer abastecer? "))
-litros = valor_reais / valor_litro
-print("com R$ %.2f o veiculo vai receber %.2f litros." % (valor_reais, litros))
