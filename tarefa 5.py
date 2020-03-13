acdc7453d054cfcfa11815218fd4d3d9aa40cffd
Gjunior17 <61851805+Gjunior17@users.noreply.github.com>
2020-03-13 19:50:10 -0300

    Delete tarefa 5.py

--- tarefa 5.py
+++
@@ -1,4 +0,0 @@
-divida = float(input('Qual o valor inicial da divida? '))
-dias = int(input('Quantos dias você atrasou a divida? '))
-juros = divida + (dias * 0.25)
-print('O valor da divida é:', juros)
